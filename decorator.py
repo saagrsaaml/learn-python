0dc21b102e7d2a5be16c4c7705e73b66236952ce
Sagar Samal <saamalsaagar@gmail.com>
2018-05-26 13:13:30 +0545

    set functions inside functions, decorator basics

---
+++ decorator.py
@@ -0,0 +1,26 @@
+# A decorator in Python is any callable Python object that is used to modify a function or a class.
+# A reference to a function "func" or a class "C" is passed to a decorator and the decorator returns a modified function or class.
+# The modified functions or classes usually contain calls to the original function "func" or class "C".
+# Two different kinds of decorators in Python:
+#    Function decorators
+#    Class decorators
+
+# Functions inside Functions
+
+def f():
+
+    def g():
+        print("Hi, it's me 'g'")
+        print("Thanks for calling me")
+
+    print("This is the function 'f'")
+    print("I am calling 'g' now:")
+    g()
+
+f()
+
+# o/p
+# This is the function 'f'
+# I am calling 'g' now:
+# Hi, it's me 'g'
+# Thanks for calling me
