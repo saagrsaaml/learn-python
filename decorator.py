b8dcfa7a3e7f01f11ea9dc86ae10da54d51b37c8
Sagar Samal <saamalsaagar@gmail.com>
2018-05-26 13:21:43 +0545

    add functions returning functions, decorator basics

--- decorator.py
+++ decorator.py
@@ -46,3 +46,24 @@ f(g)
 # Hi, it's me 'g'
 # Thanks for calling me
 # func's real name is g
+
+#------------------------------------------------------------------------------#
+print("-----------------------------------------------------------------------")
+
+# Functions returning Functions
+# The output of a function is also a reference to an object. Therefore functions can return references to function objects.
+
+def f(x):
+    def g(y):
+        return y + x + 3
+    return g
+
+nf1 = f(1)
+nf2 = f(3)
+
+print(nf1(1))
+print(nf2(1))
+
+# o/p
+# 5
+# 7
